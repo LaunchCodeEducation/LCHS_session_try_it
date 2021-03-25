4d9f0c85d4bbf2808c2b2c7a4b117fa4e77fd46c
Jim Flores <jimflores5@gmail.com>
2021-03-25 12:41:57 -0500

    Added progress to main.py.

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ def index():
     if request.method == 'POST':
         pass
     else:
-        pass
+        if 'my_list' not in session:
+            session['my_list'] = []
+        else:
+            session['my_list'].append(123)
+            session.modified = True
+        print(session, session['my_list'], session.get('my_list'))
     return render_template('index.html')
 
 if __name__ == '__main__':

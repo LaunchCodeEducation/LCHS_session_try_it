75450abd7fa5476aa987de15a6662ed0b90a7742
jimflores <jflores@launchcode.org>
2021-03-29 16:59:12 -0500

    Added code to remove session data.

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ app.secret_key = 'Chemistry'
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        new_item = request.form['new_item']
+        new_item = request.form['new_item'].title()
         current_list = session['my_list']
         if new_item not in current_list:
             current_list.append(new_item)
+        else:
+            current_list.remove(new_item)
         session['my_list'] = current_list
+        session['my_list'].sort()
     else:
         if 'my_list' not in session:
             session['my_list'] = []
